dd023fefcc9d2238975f1c898ccfd3c7435dc227
Leo-de-Riv3r <leoguti182@gmail.com>
2025-11-02 22:46:22 -0300

    Refactor MisProductos component to fetch products

--- packages/frontend/src/features/misProductos/MisProductos.jsx
+++ packages/frontend/src/features/misProductos/MisProductos.jsx
@@ -1,22 +1,25 @@
 import React, { useEffect, useState } from 'react';
 import { Button, Col, Container, Row } from 'react-bootstrap';
 import FiltrosBusquedaProductosVendedor from '../filtrosBusquedaProductosVendedor/FiltrosBusquedaProductosVendedor';
-import productosMocked from '../../mocks/productos.json'
 import CardProducto from '../../components/cards/CardProducto';
 import LoadingSpinner from '../../components/spinner/LoadingSpinner';
 import ControlPaginado from '../../components/controlPaginado/ControlPaginado';
 import CardProductoVendedor from '../../components/cards/CardProductoVendedor';
 import ErrorMessage from '../../components/errorMessage/ErrorMessage';
-import { Link, useSearchParams } from 'react-router';
+import { Link } from 'react-router';
 import { IoIosAddCircle } from "react-icons/io";
+import { useAuth } from '../../context/authContext';
+import productosService from '../../services/productos';
+import e from 'express';
 
 const MisProductos = () => {
+  const { user } = useAuth();
+  const [modalShow, setModalShow] = useState(false);
   const [productos, setProductos] = useState(null)
-  const [pagination, setPagination] = useState(productosMocked.pagination)
+  const [pagination, setPagination] = useState({})
   const [loading, setLoading] = useState(true)
   const [errorMessage, setErrorMessage] = useState("")
-  const [searchParams, setSearchParams] = useSearchParams({});
-  const [filtros, setFiltros] = useState({});
+  const [filtros, setFiltros] = useState({})
 
   const showErrorMessage = (msg) => {
     setErrorMessage(msg);
@@ -30,13 +33,7 @@ const MisProductos = () => {
       showErrorMessage("Espere a que carguen los productos")
       return
     }
-    setSearchParams({ ...filtros, page: 1 });
     setFiltros(filtros)
-    
-    setTimeout(() => {
-      setProductos(productosMocked.data)
-      setLoading(false)
-    }, 3000)
   }
 
   const handleChangePage = (page) => {
@@ -45,27 +42,37 @@ const MisProductos = () => {
       return
     }
 
-    setPagination({ ...pagination, page })
-    setSearchParams({ ...filtros, page: page });
-    alert('llamado a api de pagina ' + page)
+    setFiltros({ ...filtros, page })
+    fetchProductos()
   }
 
-  setTimeout(() => {
-    setProductos(productosMocked.data)
-    setLoading(false)
-  }, 3000)
-
-useEffect(() => {        
-        setFiltros(Object.fromEntries(searchParams.entries()));
-    }, [searchParams]);
 const handleClose = () => setModalShow(false);
+
+
+
+const fetchProductos = async () => {
+  setLoading(true)
+  try {
+    const response = await productosService.getProductos({vendedorId: user.id, ...filtros})
+    setProductos(response.data)
+    setPagination(response.pagination)
+  } catch (error) {
+    showErrorMessage("Error obteniendo productos")
+  } finally{
+    setLoading(false)
+  }
+}
+
+useEffect(() => {
+  fetchProductos()
+}, [filtros])
 
   return (
     <Container className='mt-4'>
       <ErrorMessage msg={errorMessage} />
       <Row>
         <Col lg={3} md={5} xs={12} className="mb-4">
-          <FiltrosBusquedaProductosVendedor onSubmit={handleFiltrar} filtrosActuales={Object.fromEntries(searchParams.entries())}/>
+          <FiltrosBusquedaProductosVendedor onSubmit={handleFiltrar} filtrosActuales={filtros}/>
         </Col>
         <Col lg={9} md={7} xs={12}>
           <ControlPaginado onPageChange={handleChangePage} pagination={pagination}></ControlPaginado>
